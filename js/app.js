4036a1ac86edd0527f4d63d2e983cdb3d8e9fe37
SachinOswal73 <sachin8973@gmail.com>
2022-12-10 11:46:06 +0530

    committed

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var z = 16;
+if ( z % 3 === 0){
+    // document.write("Yes, the number " + z + " is divisible by 3");
+    console.log('divisible by 3');
+}
+else {
+    // document.write("No, The number " + z + " is not divisible by 3");
+    console.log('Not divisible by 3');
+}
